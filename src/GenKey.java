97b3b058a8cb75145c37ca9026d0935907894d0c
David <tonage72@gmail.com>
2021-11-28 21:20:15 -0500

    11-28-21 Adding new class for creating key

--- src/GenKey.java
+++ src/GenKey.java
@@ -1,10 +1,15 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class GenKey {
 
@@ -13,5 +18,28 @@ public class GenKey {
     public GenKey() throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
         message = "Generate Key Class Here";
         System.out.println(message);
+    }
+
+    public void genAES() throws NoSuchAlgorithmException {
+        KeyGenerator keygen = KeyGenerator.getInstance("AES");
+        SecretKey key = keygen.generateKey();
+        
+        try {
+            File myObj = new File("AESKey.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred in file creation.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter("AESKey.txt");
+        }
+
     }
 }
